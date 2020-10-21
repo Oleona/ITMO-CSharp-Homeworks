257d9ad849de5aabc0887cb0644e9e6a991b6fa4
Oleona <Ole-onna@mail.ru>
2020-10-21 20:28:17 +0300

    Adding first homework

---
+++ TriangleSquare/TriangleSquare/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TriangleSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Input triangle perimeter");
+            double Perimeter = double.Parse(Console.ReadLine());
+
+            double Square = Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - Perimeter / 3) * (Perimeter / 2 - Perimeter / 3) * (Perimeter / 2 - Perimeter / 3));
+            double SquareVariant = Math.Sqrt(3) / 4 * (Perimeter / 3) * (Perimeter / 3);
+            Console.WriteLine($"\tside      result  \n\t{Perimeter / 3:F2}      {Square:F2}\n");
+            Console.WriteLine($"\tside      result  \n\t{Perimeter / 3:F2}      {SquareVariant:F2}");
+            Console.ReadKey();
+        }
+    }
+}
