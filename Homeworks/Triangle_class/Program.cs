893f4e7e5818df1b5371e95c0e49cab190f8aa26
Oleona <Ole-onna@mail.ru>
2020-11-01 11:10:58 +0300

    Решение 6 задачи

---
+++ Homeworks/Triangle_class/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Triangle_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Input first side of triangle:  ");
+            float side1 = float.Parse(Console.ReadLine());
+            Console.WriteLine(" Input second side of triangle:  ");
+            float side2 = float.Parse(Console.ReadLine());
+            Console.WriteLine(" Input third side of triangle:  ");
+            float side3 = float.Parse(Console.ReadLine());
+
+            try
+            {
+                Triangle triangle1 = new Triangle(side1, side2, side3);
+                double perimeter = triangle1.getPerimeter(side1, side2, side3);
+                double area = triangle1.getArea(side1, side2, side3);
+
+                triangle1.PrintSides();
+                Console.WriteLine($" Perimeter is {perimeter}");
+                Console.WriteLine($" Area is {area}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
